a8a35cd2705776b0bfbc8aa8b0ba50523eb84102
rkoko <rinda.ko@gmail.com>
2017-06-06 10:29:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 class Application < Sinatra::Base
-  get '/' do		
-    erb :index		
-  end		
-  
-  post '/greet' do		
-    erb :greet		
+  get '/' do
+    erb :index		#deliver this file to user's browser 
+  end
+
+  post '/greet' do
+    erb :greet
   end
 end
-
